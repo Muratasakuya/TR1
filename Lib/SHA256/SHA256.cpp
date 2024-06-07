27d17849113f2c702f5fc0bca57ceb24f93a85c8
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2024-06-07 11:17:25 +0900

    OpenSLLライブラリの追加

--- Lib/SHA256/SHA256.cpp
+++ Lib/SHA256/SHA256.cpp
@@ -1,1 +1,30 @@
 #include "SHA256.h"
+
+#include <openssl/sha.h>
+#include <sstream>
+#include <iomanip>
+
+//============================================================
+// ハッシュ値を生成する関数
+//============================================================
+std::string SHA256::CreateHash(const std::string& input) {
+
+	// Create a SHA256 context
+	SHA256_CTX context;
+	SHA256_Init(&context);
+
+	// Add the data to be hashed
+	SHA256_Update(&context, input.c_str(), input.size());
+
+	// Finalize the hash and get the result
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+	SHA256_Final(hash, &context);
+
+	// Convert the hash to a hex string
+	std::stringstream ss;
+	for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+		ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+	}
+
+	return ss.str();
+}
