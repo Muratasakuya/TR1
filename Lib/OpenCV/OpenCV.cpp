7d2e52dce830b37480969fa147e76c3e06cdab5e
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2024-06-07 10:15:28 +0900

    エラーが出なくなった

--- Lib/OpenCV/OpenCV.cpp
+++ Lib/OpenCV/OpenCV.cpp
@@ -14,10 +14,17 @@ OpenCV::OpenCV() {
 void OpenCV::Initialize() {
 
 	// ウィンドウの名前
-	//cv::namedWindow("OpenCV Window", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("OpenCV Window", cv::WINDOW_AUTOSIZE);
 
 	// カメラキャプチャ
 	cv::VideoCapture cap(0);
+
+	// カメラが開けなかったら
+	if (!cap.isOpened()) {
+
+		// エラーメッセージを表示する
+		std::cerr << "Failed to open camera!" << std::endl;
+	}
 
 	// カメラウィンドウのサイズ
 	cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);  // 横幅
@@ -36,8 +43,13 @@ void OpenCV::Initialize() {
 //============================================================
 void OpenCV::Update() {
 
+
+
+
 	// ウェブカメラからの画像データの読み込み
 	cap_ >> frame_;
+
+
 
 	/*-----------------------------------------------------------------------------*/
 
@@ -59,84 +71,84 @@ void OpenCV::Update() {
 
 
 
-	//// BGRからHSVへ変換
-	//// BGR : Blue Green Red || HSV : Hue(色相) Saturation(彩度) Value(明度)
-	//// 変換元の入力画像から変換後の出力画像に変換
-	//cv::cvtColor(frame_, hsvFrame_, COLOR_BGR2HSV);
+	// BGRからHSVへ変換
+	// BGR : Blue Green Red || HSV : Hue(色相) Saturation(彩度) Value(明度)
+	// 変換元の入力画像から変換後の出力画像に変換
+	cv::cvtColor(frame_, hsvFrame_, COLOR_BGR2HSV);
 
-	//// 赤色の範囲をマスクする(2つの範囲を設定)
-	//// 色相下限
-	//cv::Scalar lowerRed1(0, 120, 70);
-	//// 色相上限
-	//cv::Scalar upperRed1(10, 255, 255);
-	//// 設定したマスク範囲の出力
-	//cv::inRange(hsvFrame_, lowerRed1, upperRed1, mask1_);
+	// 赤色の範囲をマスクする(2つの範囲を設定)
+	// 色相下限
+	cv::Scalar lowerRed1(0, 120, 70);
+	// 色相上限
+	cv::Scalar upperRed1(10, 255, 255);
+	// 設定したマスク範囲の出力
+	cv::inRange(hsvFrame_, lowerRed1, upperRed1, mask1_);
 
-	//// 色相下限
-	//cv::Scalar lowerRed2(170, 120, 70);
-	//// 色相上限
-	//cv::Scalar upperRed2(180, 255, 255);
-	//// 設定したマスク範囲の出力
-	//cv::inRange(hsvFrame_, lowerRed2, upperRed2, mask2_);
+	// 色相下限
+	cv::Scalar lowerRed2(170, 120, 70);
+	// 色相上限
+	cv::Scalar upperRed2(180, 255, 255);
+	// 設定したマスク範囲の出力
+	cv::inRange(hsvFrame_, lowerRed2, upperRed2, mask2_);
 
-	//// 2つのマスクを結合
-	//cv::bitwise_or(mask1_, mask2_, mask_);
-
-
-
-	///*-----------------------------------------------------------------------------*/
+	// 2つのマスクを結合
+	cv::bitwise_or(mask1_, mask2_, mask_);
 
 
 
-	//// ノイズ除去のためにマスクを処理
-	//cv::erode(mask_, mask_, cv::Mat(), cv::Point(-1, -1), 2);
-	//cv::dilate(mask_, mask_, cv::Mat(), cv::Point(-1, -1), 2);
+	/*-----------------------------------------------------------------------------*/
 
 
 
-	///*-----------------------------------------------------------------------------*/
+	// ノイズ除去のためにマスクを処理
+	cv::erode(mask_, mask_, cv::Mat(), cv::Point(-1, -1), 2);
+	cv::dilate(mask_, mask_, cv::Mat(), cv::Point(-1, -1), 2);
 
 
 
-	//// 赤い場所の輪郭部分を検出する
-	//std::vector<std::vector<cv::Point>> contours;
-	//cv::findContours(mask_, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
+	/*-----------------------------------------------------------------------------*/
 
-	//// 最大の輪郭を見つける
-	//// 最大の輪郭のインデックス、-1は見つかっていない状態
-	//int largestContourIndex = -1;
-	//// 最大面積
-	//double largestArea = 0;
-	//for (size_t i = 0; i < contours.size(); i++) {
 
-	//	// 輪郭の面積の計算
-	//	double area = cv::contourArea(contours[i]);
 
-	//	if (area > largestArea) {
+	// 赤い場所の輪郭部分を検出する
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(mask_, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
-	//		// 最大面積の更新
-	//		largestArea = area;
+	// 最大の輪郭を見つける
+	// 最大の輪郭のインデックス、-1は見つかっていない状態
+	int largestContourIndex = -1;
+	// 最大面積
+	double largestArea = 0;
+	for (size_t i = 0; i < contours.size(); i++) {
 
-	//		// 現在の輪郭のインデックスiを保存
-	//		largestContourIndex = static_cast<int>(i);
-	//	}
-	//}
+		// 輪郭の面積の計算
+		double area = cv::contourArea(contours[i]);
 
-	//// 最大の輪郭が見つかった場合、その重心を計算
-	//if (largestContourIndex != -1) {
+		if (area > largestArea) {
 
-	//	// 輪郭のモーメント計算、モーメントは、形状の中心や分布を示す統計量
-	//	cv::Moments moments = cv::moments(contours[largestContourIndex]);
+			// 最大面積の更新
+			largestArea = area;
 
-	//	// 中心座標の計算
-	//	centerPos_.x = static_cast<int>(moments.m10 / moments.m00);
-	//	centerPos_.y = static_cast<int>(moments.m01 / moments.m00);
+			// 現在の輪郭のインデックスiを保存
+			largestContourIndex = static_cast<int>(i);
+		}
+	}
 
-	//	isFound_ = true;
-	//} else {
+	// 最大の輪郭が見つかった場合、その重心を計算
+	if (largestContourIndex != -1) {
 
-	//	isFound_ = false;
-	//}
+		// 輪郭のモーメント計算、モーメントは、形状の中心や分布を示す統計量
+		cv::Moments moments = cv::moments(contours[largestContourIndex]);
+
+		// 中心座標の計算
+		centerPos_.x = static_cast<int>(moments.m10 / moments.m00);
+		centerPos_.y = static_cast<int>(moments.m01 / moments.m00);
+
+		isFound_ = true;
+	} else {
+
+		isFound_ = false;
+	}
 }
 
 
